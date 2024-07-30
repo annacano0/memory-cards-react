4a563550e014bbedb423911c563c5bc36782124d
annacano0 <anna.elisabet5@gmail.com>
2024-07-30 09:19:39 +0200

    REFACTOR: clean Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,4 @@
-import {useEffect} from 'react'
 export default function Card({ cardData, isMatched, handleCardReveal, isCovered}) {
-
-    useEffect(() => {
-        function hola() {
-           console.log("update de isCovered ")
-        }
-        hola();
-    }, [isCovered]);
 
     function handleClickCard() {
         if(!isMatched&&isCovered){
@@ -16,9 +8,9 @@ export default function Card({ cardData, isMatched, handleCardReveal, isCovered}
 
     return (
         <>
-            <button onClick={handleClickCard} 
+            <div onClick={handleClickCard} 
             className={`board-card ${(isCovered) ? 'unflipped' : 'flipped'}`} 
-            data-testid={`board-card${cardData.y + 1}-${cardData.x + 1}`} >{!isCovered && cardData.cardNumber}</button>
+            data-testid={`board-card${cardData.y + 1}-${cardData.x + 1}`} >{!isCovered && cardData.cardNumber}</div>
         </>
     )
 }
